2245183a0b417196495a7bf85bfd19daccbbf4c4
vominh1 <vominhday1@gmail.com>
2024-07-17 23:25:10 +0700

    thaydoigiaodien
    thay đổi giao điện trang quản trị

--- Areas/PrivatePages/Controllers/ProductsController.cs
+++ Areas/PrivatePages/Controllers/ProductsController.cs
@@ -42,6 +42,7 @@ namespace ShopTechNoLoGy.Areas.PrivatePages.Controllers
         {
             List<sanPham> l = db.sanPhams.Where(x => x.daDuyet == true).ToList<sanPham>();
             ViewData["DanhSachSP"] = l;
+            
         }
         [HttpGet]
         private List<SelectListItem> GetMaLoaiList()
