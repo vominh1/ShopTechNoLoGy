53cfbce5efafebcd9cd44216e759cda91bf4d890
vominh1 <vominhday1@gmail.com>
2025-03-19 21:41:51 +0700

    update Startup
    xây dựng cấu hình google Authentication

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,41 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using System.Configuration;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
 using Owin;
 
-[assembly: OwinStartup(typeof(ShopTechNoLoGy.StartupOwin))]
+[assembly: OwinStartup(typeof(ShopTechNoLoGy.Startup))]
 
 namespace ShopTechNoLoGy
 {
-    public partial class StartupOwin
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            //AuthStartup.ConfigureAuth(app);
+            
+            app.UseKentorOwinCookieSaver();// bắt buộc phải có dòng này nếu không có GoogleCallback() is null 
+            // Cấu hình xác thực bằng Cookie
+            app.UseCookieAuthentication(new CookieAuthenticationOptions {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Login/Index"),
+                ExpireTimeSpan = TimeSpan.FromMinutes(30),
+                SlidingExpiration = true
+            });
+
+            // Dòng này rất quan trọng để giữ thông tin người dùng khi xác thực từ Google
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Cấu hình Google Authentication
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions {
+                ClientId = ConfigurationManager.AppSettings["GoogleClientId"],
+                ClientSecret = ConfigurationManager.AppSettings["GoogleClientSecret"],
+                CallbackPath = new PathString("/signin-google"),
+                Scope = { "email", "profile", "openid" }
+            });
         }
+
+
     }
 }
